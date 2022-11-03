5cb5fa5432497c19d57b74dc3e0c819f1783c605
Ryan Broome <ryan@MacBook-Pro.local>
2022-11-03 00:38:31 -0400

    initial commit

---
+++ ES5app.js
@@ -0,0 +1,15 @@
+const PI = 3.14;
+('What is the difference between var and let');
+('var can be redeclared and reassigned, let cannot be redeclared let is only available in scope of declaration');
+
+('var and const');
+('var can be redeclared and reassigned, const can not be redeclared or re assigned, const has block scope');
+
+('let and const');
+('let can be reassigned const cannot ');
+
+('what is hoisting');
+('hoisting is declaring a variable before initializing it, the browser declaring variables before running code gives access to undefined but the variable is declared');
+
+('ES2015 Global Constants');
+const pI = 3.14;
